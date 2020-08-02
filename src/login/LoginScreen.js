44bd09c43018c85b13344b6e15e41760e1986ebb
Hong <39299265+hsk1321@users.noreply.github.com>
2020-08-02 19:37:46 +0900

    login form

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./LoginScreen.css";
+import { IoIosContact, IoIosLock } from "react-icons/io";
 
 class LoginScreen extends Component {
     state = {
@@ -12,7 +13,10 @@ class LoginScreen extends Component {
         return (
             <div className="login__form">
                 <div className="input__form">
-                    <div className="input__form__txt">ID</div>
+                    <div className="icons">
+                        <IoIosContact size={30} />
+                    </div>
+
                     <input
                         id="id"
                         className="input__box"
@@ -21,7 +25,9 @@ class LoginScreen extends Component {
                     />
                 </div>
                 <div className="input__form">
-                    <div className="input__form__txt">PASSWORD</div>
+                    <div className="icons">
+                        <IoIosLock size={30} />
+                    </div>
                     <input
                         id="pwd"
                         className="input__box"
@@ -33,7 +39,9 @@ class LoginScreen extends Component {
                     className="submit__btn"
                     type="submit"
                     onKeyPress={this.login}
-                />
+                >
+                    Login
+                </button>
             </div>
         );
     };
