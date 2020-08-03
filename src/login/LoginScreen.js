eec9c27a941aab37684e3c7fadf795bbc9a7cdf6
Hong <39299265+hsk1321@users.noreply.github.com>
2020-08-03 20:52:05 +0900

    login form

--- src/login/LoginScreen.js
+++ src/login/LoginScreen.js
@@ -6,22 +6,54 @@ class LoginScreen extends Component {
     state = {
         id: "",
         pwd: "",
+        login_flag: false,
     };
-    login = () => {};
+
+    textChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    login = () => {
+        const { id, pwd } = this.state;
+
+        if (id === "" || pwd === "") {
+            alert("Input your ID or Password");
+        } else {
+            if (id === "admin" && pwd === "1321") {
+                alert("wellcome! admin");
+                this.setState({
+                    id: "",
+                    pwd: "",
+                    login_flag: true,
+                });
+            } else {
+                this.setState({
+                    id: "",
+                    pwd: "",
+                });
+                console.log(this.state.id, this.state.pwd);
+            }
+        }
+    };
 
     render = () => {
+        const { id, pwd } = this.state;
         return (
             <div className="login__form">
                 <div className="input__form">
                     <div className="icons">
                         <IoIosContact size={30} />
                     </div>
-
                     <input
                         id="id"
+                        name="id"
                         className="input__box"
                         type="text"
                         placeholder="ID"
+                        onChange={this.textChange}
+                        value={id}
                     />
                 </div>
                 <div className="input__form">
@@ -30,18 +62,25 @@ class LoginScreen extends Component {
                     </div>
                     <input
                         id="pwd"
+                        name="pwd"
                         className="input__box"
                         type="password"
                         placeholder="Password"
+                        onChange={this.textChange}
+                        value={pwd}
                     />
                 </div>
-                <button
-                    className="submit__btn"
-                    type="submit"
-                    onKeyPress={this.login}
-                >
+                <button className="submit__btn" onClick={this.login}>
                     Login
                 </button>
+                <div className="login__screen__contents">
+                    <a href="/" className="link__contents">
+                        Forgot?
+                    </a>
+                    <a href="/" className="link__contents">
+                        Sign in?
+                    </a>
+                </div>
             </div>
         );
     };
